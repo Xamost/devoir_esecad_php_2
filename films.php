c43b2b295ddefffce17abc4af4e9cca3d7ae8e8c
tloudoux <xamost.game@gmail.com>
2023-05-01 11:48:53 +0200

    Documentation

--- films.php
+++ films.php
@@ -1,4 +1,13 @@
 <?php
+/*
+#
+#               file name : films.php
+#               autor : Thomas Loudoux
+#
+*/
+
+
+    // liste de tous les films (prototype d'une base de donnée)
     $films_info_list = [
         0 => ['year' => '2001', 'name' => 'Le Seigneur des anneaux : La Communauté de l\'anneau', 'duration' => '2h58min', 'grade' => '8.8/10'],
         1 => ['year' => '2002', 'name' => 'Le Seigneur des anneaux : Les Deux Tours', 'duration' => '2h59min', 'grade' => '8.8/10'],
@@ -8,10 +17,13 @@
         5 => ['year' => '2003', 'name' => 'Kill Bill: Volume I', 'duration' => '1h51min', 'grade' => '8.2/10'],
         6 => ['year' => '1977', 'name' => 'La guerre des étoiles', 'duration' => '2h01min', 'grade' => '8.6/10']
     ];
-    if(!empty($_GET))
+    // On vérifie si on as des paramètre dans la requète, et si ces paramètre utilise la méthode GET
+    if(!empty($_GET) and !empty($_GET['year'])) // si il n'est pas vide et que le paramètre est l'année
     {
+        // On déclare la variable year et on lui donne la valeur de l'année. (rend le code plus lisible)
         $year = $_GET['year'];
     }else{
+        // Sinon on renvoie l'utilisateur sur le landing.
         header('Location: ../index.php');
         exit();
     }
@@ -27,17 +39,19 @@
     <title><?php echo $year;?> Devoir 2 FILM </title>
 </head>
 <body>
+    <h1>Voici les films pour l'année : <?php echo $year ?></h1>
+    <ul>
     <?php
-        echo "<h1> Voici les films pour l'année : " .$year . "</h1></br>";
-        echo "<ul>";
-        foreach ($films_info_list as $key => $values)
+        // génération de la liste des films qui correspondent a l'année entré par l'utilisateur
+        foreach ($films_info_list as $key => $values) // On traverse la liste des films
         {
-            if ($year == $values['year'])
+            if ($year == $values['year']) // si l'année entré correspond a l'année du film "i"
             {
+                // on génère l'élément de la liste HTML avec les bonne valeur a l'interieur.
                 echo "<li><h3>" . $values['name'] . " | d'une durée de : " . $values['duration'] . " | avec une note de : " . $values['grade'] . "</h3></h3></li></br>";
             }
         }
-        echo "</ul>";
     ?>
+    </ul>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+/*
+#
+#               file name : index.php
+#               autor : Thomas Loudoux
+#
+*/
+
+
+    // liste de tous les films (prototype d'une base de donnée)
     $films_info_list = [
         0 => ['year' => '2001', 'name' => 'Le Seigneur des anneaux : La Communauté de l\'anneau', 'duration' => '2h58min', 'grade' => '8.8/10'],
         1 => ['year' => '2002', 'name' => 'Le Seigneur des anneaux : Les Deux Tours', 'duration' => '2h59min', 'grade' => '8.8/10'],
@@ -8,15 +17,17 @@
         5 => ['year' => '2003', 'name' => 'Kill Bill: Volume I', 'duration' => '1h51min', 'grade' => '8.2/10'],
         6 => ['year' => '1977', 'name' => 'La guerre des étoiles', 'duration' => '2h01min', 'grade' => '8.6/10']
     ];
+    // déclaration de la liste des année des films enregistrer Non obligatoire mais ça rend le code plus lisible plus tard Théoriquement on prendrais la valeur la plus ancienne et on incrémenterais automatiquement pour générer ce genre de liste avec une vrais base de donnée.
     $years_list = [];
-    foreach ($films_info_list as $key => $values)
+    foreach ($films_info_list as $key => $values) // on traverse la liste des films
     {
-        if ($values['year'])
+        if ($values['year'] and !empty($values['year']) and $values['year'] != "") // si il y a une valeur année on l'ajoute à la liste, cela évite toute erreur qui ferais planter le système
         {
-            $years_list[] = $values['year'];
-            $years_list = array_unique($years_list);
+            $years_list[] = $values['year'];  // on ajoute l'année a la liste des années
+            $years_list = array_unique($years_list); // on retire tous les doublon de la liste pour afficher qu'un seul lien si on a plusieur films ayant la même année.
         }
     }
+
 ?>
 
 <!doctype html>
@@ -34,8 +45,9 @@
     </header>
     <ul>
         <?php
-            foreach ($years_list as $key => $value)
+            foreach ($years_list as $key => $value) // génération de la liste de lien
             {
+                // j'ai ajouter des class pour facilité un ajout de style a la page, le lien va chercher le fichier films.php qui est à la racine du projet, le seul paramètre est l'année puisqu'on applique un filtre unique pour la recherche de films.
                 echo "<li class='generated_list'><a class='generated_link' target='_blank' href='./films.php?year=" . $value . "'>". $value."</a></li>";
             }
         ?>
